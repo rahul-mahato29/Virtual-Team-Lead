42d09c957a0a15c894d2bdd739a5acf50264091f
Rahul Mahato <mahatorahul2901@gmail.com>
2024-01-02 01:07:11 +0530

    testing DB

--- Ticket1/Implementation/routes/login.js
+++ Ticket1/Implementation/routes/login.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const userModel = require('../models/userModel');
 
 // login route
 router.post('/', (req, res) => {
   // Implement user login logic here
   res.send('User login endpoint');
+
+  //creating database
+  const user = new userModel({
+    username: 'Rahul Mahato',
+    email: 'rahul@gmail.com',
+    password: 'rahul123'
+  });
+
+  user.save();
 });
 
 module.exports = router;
